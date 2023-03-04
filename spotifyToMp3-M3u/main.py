9c504c0a95a2f3b145cea5608f5f9faed1884d5e
zigzag1001 <72932714+zigzag1001@users.noreply.github.com>
2023-03-03 19:17:59 -0500

    Delete spotifyToMp3-M3u directory

--- spotifyToMp3-M3u/main.py
+++
@@ -1,6 +0,0 @@
-from get_spot_access_token import getAccessToken
-from get_all_playlists import getAllPlaylists
-
-headers = getAccessToken()
-
-getAllPlaylists(headers)
--- spotifyToMp3-M3u/run_every_24h.sh
+++
@@ -1,3 +0,0 @@
-time bash update_all.sh
-echo $(date)
-while sleep 43200; do time bash update_all.sh; echo $(date); done
