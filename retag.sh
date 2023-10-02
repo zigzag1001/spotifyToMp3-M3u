b0b62e6479a6a47d7ab7315b61691ba6bf749903
Peter S <72932714+zigzag1001@users.noreply.github.com>
2023-10-02 00:46:02 -0400

    delete files that no longer exist from tracking file

--- retag.sh
+++ retag.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+# Change to the directory containing the mp3 files
 cd ../mp3
 
 processed_file="processed_files.txt"
 
-# Loop through mp3 files with spaces and special characters handled
+# Loop through the entries in processed_files.txt
+while read -r filename; do
+    # If a file does not exist, remove the entry from processed_files.txt
+    if [[ ! -f $filename ]]; then
+        echo "Removing $filename from $processed_file"
+        sed -i "/$filename/d" "$processed_file"
+    fi
+done < "$processed_file"
+
+# Loop through the mp3 files in the directory
 find . -name "*.mp3" -print0 | while IFS= read -r -d '' file; do
     filename=$(basename "$file")
 
+    # If the file is not present in processed_files.txt, retag it
     if ! grep -qFx "$filename" "$processed_file"; then
-        main_artist=$(eyeD3 "$file" | grep "^artist:" | cut -d ':' -f 2 | cut -d '/' -f 1 | xargs)
+        main_artist=$(eyeD3 "$file" | sed -n 's/^artist: \(.*\)/\1/p' | cut -d '/' -f 1)
         eyeD3 --artist="$main_artist" "$file"
         echo "$filename" >> "$processed_file"
     fi
